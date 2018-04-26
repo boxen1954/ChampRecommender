7e81049d6efd2c97575430cfb134122a2c470da3
boxen1954 <ethanpathlete@gmail.com>
2018-04-26 11:10:52 -0400

    Add files via upload
    Does not work yet. Will work on file parsing today.

---
+++ ProjectReccWatcher.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 15 19:22:27 2018
+
+@author: Ethan Paulsen
+"""
+
+#This is a test using the riotwatcher library.
+
+from riotwatcher import RiotWatcher as rw
+from pathlib import Path
+import requests
+
+def checkFile( my_region ):
+    api_key="RGAPI-0c72f3b7-fccf-4a60-b112-114e69fa3044"
+    watcher = rw(api_key)
+    print( "Watcher Established." )
+    print()
+    print( "-------------------------------------------------" )
+    p = Path(".")
+    myfile = p / "static_champ_list.csv"
+    if myfile.exists():
+        fp = open( "static_champ_list.csv", "r" )
+        fp.close()
+    else:
+    
+        champURL = "https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&champData=stats&tags=tags&tags=info&api_key=" + api_key
+        champNameJSON = requests.get( champURL ).json()
+        #if "status" in champNameJSON:
+        #   print( "Failure to retrieve champion statistics." )
+        #  print( "Error:", *champNameJSON["status"].values(), sep = "  ")
+        fp = open( "static_champ_list.csv", "w" )
+        print( champNameJSON )
+        for champ in champNameJSON["data"]:
+            fp.write( str(champ["id"]) + "," )
+            fp.write(champ["name"] + "," );
+            if champ["tags"].size() > 1:
+                for typ in champ["tags"]:
+                    fp.write( typ + ",")
+            else:
+                fp.write( *champ["tags"] +",null," )
+            count = 0;
+            for item in champ["info"].values():
+                if( count != 2 ):
+                    fp.write( str(item) + "," )
+                else:
+                    fp.write( str(item) + "\n" )
+        fp.close()
+        print( "Error creating object" )
+        return 0
+    print( "Static champion list found." )
+    print( "\n\n---------------------------------------\n\n" )
+
+def getInfo():
+    watcher = rw("RGAPI-0c72f3b7-fccf-4a60-b112-114e69fa3044")
+    regionList = ["na1", "euw1", "eun1", "kr", "ru", "jp1", "oc1", "tr1", "la1", "la2" ]
+    my_region = input( "Enter your region, press enter to quit. For a list of regions, please type region: " )
+    print()
+    while my_region == "region":
+        print( "The different regions are:" )
+        print( *regionList, sep="\n" )
+        my_region = input( "Enter your region, press enter to quit. For a list of regions, please type region: " )
+    summonerName = input( "Please enter a name, hit Enter to quit: " )
+    print()
+
+    if summonerName == "":
+        return ""
+
+    checkFile( my_region )
+
+    me = watcher.summoner.by_name( my_region, summonerName )
+    
+    print("Summoner found.\n\n" )
+    print( me )
+    print("\n\n------------------------------------------\n\n" )
+    recentMatches = watcher.match.matchlist_by_account_recent( my_region, me["accountId"] )
+    
+    gameId = []
+    champPicked = []
+    infoRetrieved = {}
+    for game in recentMatches["matches"]:
+        gameId.append( game["gameId"] )
+        champPicked.append( game["champion"] )
+    
+    fp = open( "static_champ_list.csv", "r" )
+    for line in fp:
+        print( line )
+    #for x in range( 0, 20 ):
+#    gamePlayed = watcher.match.by_id( my_region, gameId[0] );
+   #print( gamePlayed )
+  #  for line in fp:
+   #     if champPicked[0] in line:
+    #        gameData = {}
+     #       string = fp.readline( line ).split(",");
+      #      gameData.update( { "champion" : string[1] } )
+       #     if "0" in line:
+        #        gameData.update( { "tag": string[2] } )
+         #       gameData.update( { "info" : [ string[4], string[5], string[6], string[7] ] } )
+           # else:
+          #      gameData.update( {"tags", [string[3], string[4]] })
+               # gameData.update( { "info" : [ string[5], string[6], string[7], string[8] ] } )
+          #  print( gameData )
+          #  print()
+          #  print()
+          #  infoRetrieved.update( { 0 : gameData } )
+                
+    
+getInfo()
