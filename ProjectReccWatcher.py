c32bfc00906ae5c642bc5ce3263328f84854209f
GodKing630 <38386090+GodKing630@users.noreply.github.com>
2018-04-30 17:29:44 -0400

    added name to static and grabbed gameID and champID from recent matches

--- ProjectReccWatcher.py
+++ ProjectReccWatcher.py
@@ -26,12 +26,14 @@ def checkFile( my_region ):
     #  print( "Error:", *champNameJSON["status"].values(), sep = "  ")
     #fp = open( "static_champ_list.csv", "w" )
     with open("static_champ_list.csv" , "w") as fp:
-        fp.write('primary,secondary,attack,defense,magic,difficulty')
+        fp.write('id,name,primary,secondary,attack,defense,magic,difficulty' + '\n')
         print( champNameJSON["data"] )
         for champ in champNameJSON["data"].values():
             print('champ: ' , champ)
             #fp.write( str(champ["id"]) , "," )
             #fp.write(champ["name"] , "," );
+            fp.write(str(champ['id']) + ',')
+            fp.write(champ['name'] + ',')
             if len(champ["tags"]) > 1:
                 for typ in champ["tags"]:
                     fp.write( typ + ",")
@@ -73,35 +75,40 @@ def getInfo():
     print( me )
     print("\n\n------------------------------------------\n\n" )
     recentMatches = watcher.match.matchlist_by_account_recent( my_region, me["accountId"] )
-    
-    gameId = []
-    champPicked = []
-    infoRetrieved = {}
-    for game in recentMatches["matches"]:
-        gameId.append( game["gameId"] )
-        champPicked.append( game["champion"] )
-    
-    fp = open( "static_champ_list.csv", "r" )
-    for line in fp:
-        print( line )
-    #for x in range( 0, 20 ):
+    print("recent: " , recentMatches)
+#    gameId = []
+#    champPicked = []
+#    infoRetrieved = {}
+#    for game in recentMatches["matches"]:
+#        gameId.append( game["gameId"] )
+#        champPicked.append( game["champion"] )
+#    
+#    fp = open( "static_champ_list.csv", "r" )
+#    for line in fp:
+#        print( line )
+#    for x in range( 0, 20 ):
 #    gamePlayed = watcher.match.by_id( my_region, gameId[0] );
-   #print( gamePlayed )
-  #  for line in fp:
-   #     if champPicked[0] in line:
-    #        gameData = {}
-     #       string = fp.readline( line ).split(",");
-      #      gameData.update( { "champion" : string[1] } )
-       #     if "0" in line:
-        #        gameData.update( { "tag": string[2] } )
-         #       gameData.update( { "info" : [ string[4], string[5], string[6], string[7] ] } )
-           # else:
-          #      gameData.update( {"tags", [string[3], string[4]] })
-               # gameData.update( { "info" : [ string[5], string[6], string[7], string[8] ] } )
-          #  print( gameData )
-          #  print()
-          #  print()
-          #  infoRetrieved.update( { 0 : gameData } )
+#   print( gamePlayed )
+#    for line in fp:
+#        if champPicked[0] in line:
+#            gameData = {}
+#            string = fp.readline( line ).split(",");
+#            gameData.update( { "champion" : string[1] } )
+#            if "0" in line:
+#                gameData.update( { "tag": string[2] } )
+#                gameData.update( { "info" : [ string[4], string[5], string[6], string[7] ] } )
+#            else:
+#                gameData.update( {"tags", [string[3], string[4]] })
+#                gameData.update( { "info" : [ string[5], string[6], string[7], string[8] ] } )
+#            print( gameData )
+#            print()
+#            print()
+#            infoRetrieved.update( { 0 : gameData } )
                 
-    fp.close()
+#    fp.close()
+    for i in range(len(recentMatches['matches'])):
+        gameId = recentMatches['matches'][i]['gameId']
+        champId = recentMatches['matches'][i]['champion']
+        print('gameId: ' + str(recentMatches['matches'][i]['gameId']))
+        print('champId: ' + str(recentMatches['matches'][i]['champion']))
 getInfo()
